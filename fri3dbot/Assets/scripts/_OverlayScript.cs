998fe4bdd52070f469bebc0105361e49ae53239d
kefcom <kefcom@hotmail.com>
2018-03-30 11:09:53 +0200

    Overlay script changed, security number added. removed scrolling effect for overlaytext.

--- fri3dbot/Assets/scripts/_OverlayScript.cs
+++ fri3dbot/Assets/scripts/_OverlayScript.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class _OverlayScript : MonoBehaviour {
+public class _OverlayScript : MonoBehaviour
+{
 
     public GameObject canvasPrefab = null;
     public float lastPosition = 0f;
     private GameObject thisCanvas = null;
     private GameObject thisTextField = null;
+    private GameObject thisCodeField = null;
     private string thisScene = null;
+    public int currentSecurityCode = 123;
+    public int lastSecurityCode = 321;
+    public float securityCodeTime = 60f;
+    public string overlayText = "";
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         if (SceneManager.GetActiveScene().name == "_startup")
         {
             // don't destroy this object
@@ -21,47 +29,57 @@ public class _OverlayScript : MonoBehaviour {
             DontDestroyOnLoad(this);
 
         }
+        spawnOverlay();
+        generateSecurityCode();
+    }
 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name != thisScene)
+        {
+            spawnOverlay();
+        }
+
+    }
+    
+    public void spawnOverlay()
+    {
         //instantiate overlay object
-        thisCanvas = Instantiate(canvasPrefab, this.gameObject.transform.position  , this.gameObject.transform.rotation);
+        thisCanvas = Instantiate(canvasPrefab, this.gameObject.transform.position, this.gameObject.transform.rotation);
         thisCanvas.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceCamera;
         thisCanvas.GetComponent<Canvas>().worldCamera = Camera.main;
 
         //OverlayText
-        thisTextField = GameObject.Find("overlayText").gameObject;
-        thisTextField.GetComponent<Text>().text = "test";
+        thisTextField = GameObject.Find("bottomText").gameObject;
+        thisTextField.GetComponent<Text>().text = overlayText;
+        thisScene = SceneManager.GetActiveScene().name;
 
-        //set position of text
-        lastPosition = Camera.main.transform.position.x + 8.7f;
-        Debug.Log(lastPosition);
-        Vector3 newPosition = new Vector3(lastPosition, thisTextField.transform.position.y, thisTextField.transform.position.z);
-        thisTextField.transform.position = newPosition;
-
+        //override colors for some faces
+        //bend-r (background is gray)
+        if (SceneManager.GetActiveScene().name.Substring(0,6) == "bend-r")
+        {
+            thisTextField.GetComponent<Text>().color = Color.black;
+            thisCodeField.GetComponent<Text>().color = Color.black;
+        }
     }
-   
 
-    // Update is called once per frame
-    void Update () {
-        if(SceneManager.GetActiveScene().name != thisScene)
-        {
-            Debug.Log("test");
-            //instantiate overlay object
-            thisCanvas = Instantiate(canvasPrefab, this.gameObject.transform.position, this.gameObject.transform.rotation);
-            thisCanvas.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceCamera;
-            thisCanvas.GetComponent<Canvas>().worldCamera = Camera.main;
+    public void generateSecurityCode()
+    {
+        lastSecurityCode = currentSecurityCode;
+        currentSecurityCode = Random.Range(100, 999);
+        //updateOverlay
+        thisCodeField = GameObject.Find("topText").gameObject;
+        thisCodeField.GetComponent<Text>().text = currentSecurityCode.ToString() ;
+        Invoke("generateSecurityCode", securityCodeTime);
+    }
 
-            //OverlayText
-            thisTextField = GameObject.Find("overlayText").gameObject;
-            thisTextField.GetComponent<Text>().text = "test";
-            thisScene = SceneManager.GetActiveScene().name;
-        }
-        lastPosition = (lastPosition - 0.1f);
-        Vector3 newPosition = new Vector3 (lastPosition, thisTextField.transform.position.y, thisTextField.transform.position.z);
-        thisTextField.transform.position = newPosition;
-        RectTransform textRectTransform = thisTextField.GetComponent<RectTransform>();
-        if(textRectTransform.offsetMax.x < -500f)
-        {
-            lastPosition = (Camera.main.transform.position.x + 8.7f);
-        }
+    public void setOverlayText(string text)
+    {
+        overlayText = text;
+        //OverlayText
+        thisTextField = GameObject.Find("bottomText").gameObject;
+        thisTextField.GetComponent<Text>().text = overlayText;
     }
 }
