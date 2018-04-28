4ddbbf51be4dfb108b1d7857ce568fbfc7148105
kefcom <kefcom@hotmail.com>
2018-04-28 10:45:27 +0200

    added missing animationscript file (again!)

---
+++ fri3dbot/Assets/scripts/_animationScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class _animationScript : MonoBehaviour {
+
+    public bool isBusy = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void end_anim()
+    {
+        isBusy = false;
+        switch (SceneManager.GetActiveScene().name.Substring(0,4))
+        {
+            //triggerReady
+            case "bend":
+                //bender script
+                GameObject.Find("bendRScriptHolder").GetComponent<bendrScript>().triggerReady();
+                break;
+            case "eeve":
+                //eeve script
+                GameObject.Find("eeveScriptHolder").GetComponent<eeveScript>().triggerReady();
+                break;
+            case "gear":
+                //gearhead script
+                GameObject.Find("gearHeadScriptHolder").GetComponent<gearheadScript>().triggerReady();
+                break;
+            case "ledF":
+                //ledFace script
+                GameObject.Find("scriptHolder").GetComponent<ledFaceScript>().triggerReady();
+                break;
+            case "robo":
+                //roboHead script
+                GameObject.Find("roboScriptHolder").GetComponent<roboFaceScript>().triggerReady();
+                break;
+            default:
+                //do nothing
+                Debug.Log("unexpected state 'default' in _animationScript.cs");
+                break;
+        }
+    }
+    public void begin_anim()
+    {
+        isBusy = true;
+    }
+}
