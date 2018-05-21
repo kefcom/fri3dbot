82ac26a896a215c780b1415c667d4f62b4e8ed05
Berend Wouters <berend.wouters@gmail.com>
2018-05-21 16:33:20 +0200

    Fixup namespacing

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Fri3dBotServer
+namespace Server
 {
     public class Startup
     {
