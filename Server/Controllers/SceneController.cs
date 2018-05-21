38144c39583c64710561e149fd42e44e85969fe1
Berend Wouters <berend.wouters@gmail.com>
2018-05-21 16:30:57 +0200

    Adds Server

---
+++ Server/Controllers/SceneController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/scenes")]
+    public class SceneController : Controller
+    {
+        private static string[] Scenes = new[]
+        {
+            "bend-r-init", "eeve-init", "gearHead_init",
+        };
+
+        [HttpGet]
+        [Route("random")]
+        public string GetScenes()
+        {
+            var rng = new Random();
+            var randomPos = rng.Next(Scenes.Length);
+            return  Scenes[randomPos];
+        }
+    }
+}
