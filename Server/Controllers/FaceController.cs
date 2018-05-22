f28ec84df9306e56f5dac2d2310930df67d1deb2
Berend Wouters <berend.wouters@gmail.com>
2018-05-22 20:37:29 +0200

    Adds migrations for storing last FaceRequests

---
+++ Server/Controllers/FaceController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assets.scripts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/faces")]
+    public class FaceController : Controller
+    {
+        private readonly IReadOnlyList<int> _newestList  = new List<int>();
+        private readonly FaceRequestContext _context;
+
+        public FaceController()
+        {
+            _context = new FaceRequestContext();
+        }
+
+        [HttpGet]
+        [Route("random")]
+        public int GetRandomFace()
+        {
+            var rng = new Random();
+            var randomPos = rng.Next(Shared.Faces.Count);
+            return randomPos;
+        }
+
+        [HttpGet]
+        [Route("newest")]
+        public async Task<int> GetNewestFace()
+        {
+            var newestItems = await _context.FaceRequests.Where(x => !x.IsCompleted).ToListAsync();
+            if (newestItems.Any())
+            {
+                var newestItem = newestItems.First();
+                newestItem.IsCompleted = true;
+                await _context.SaveChangesAsync();
+                return newestItem.RequestedFaceId;
+            }
+
+            return -1;
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public async Task AddNewFaceAsync([FromBody]FaceRequestDTO faceRequestDTO)
+        {
+            var faceRequest = new FaceRequest()
+            {
+                RequestedFaceId = faceRequestDTO.FaceId,
+                AuthorizationCode = faceRequestDTO.AuthorizationCode,
+                CreationDateTime = DateTime.UtcNow
+            };
+            _context.FaceRequests.Add(faceRequest);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public class FaceRequestDTO
+    {
+        public int FaceId { get; set; }
+        public int AuthorizationCode { get; set; }
+    }
+}
