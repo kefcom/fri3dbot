cc5c0378bd0a9f4674f1171ddc81790ecc71a1fe
Berend Wouters <berend.wouters@gmail.com>
2018-06-26 21:32:35 +0200

    Adds InMemory list

--- Server/Services/InMemoryRequestService.cs
+++ Server/Services/InMemoryRequestService.cs
@@ -8,23 +8,28 @@ namespace Server.Services
     public class InMemoryRequestService : IRequestService
     {
         private readonly List<FaceRequest> _requests;
+        private int Id { get; set; }
 
         public InMemoryRequestService()
         {
+            Id = 0;
             _requests = new List<FaceRequest>();
         }
         public async Task AddAsync(FaceRequest faceRequest)
         {
+            faceRequest.Id = Id++;
             _requests.Add(faceRequest);
         }
-        
+
         public async Task<FaceRequest> GetLatestRequestAsync()
         {
-            var newestItems = _requests.Where(x => !x.IsCompleted).ToList();
+            var newestItems = _requests
+                .Where(x => !x.IsCompleted)
+                .OrderBy(x => x.Id).ToList();
             if (newestItems.Any())
             {
                 var newestItem = newestItems.First();
-                newestItem.IsCompleted = true;
+                newestItems.ForEach(x => x.IsCompleted = true);
                 return newestItem;
             }
             return null;
